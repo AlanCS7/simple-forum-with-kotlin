711446f770727a2077ef965c79d735b63258d771
Alan Silva <alanclash2541@gmail.com>
2025-04-14 23:32:04 -0300

    feat: add initial project structure with Maven and Spring Boot setup

---
+++ src/test/kotlin/dev/alancss/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.alancss.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

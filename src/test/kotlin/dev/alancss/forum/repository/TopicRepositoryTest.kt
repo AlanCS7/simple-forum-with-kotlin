a9b75f97e95b37d7097847ddcdbe2bc27fa1f13d
Alan Silva <alanclash2541@gmail.com>
2025-06-01 19:31:04 -0300

    test: enhance TopicRepositoryTest with setup and teardown methods, and add tests for topic retrieval and report generation

--- src/test/kotlin/dev/alancss/forum/repository/TopicRepositoryTest.kt
+++ src/test/kotlin/dev/alancss/forum/repository/TopicRepositoryTest.kt
@@ -1,15 +1,21 @@
 package dev.alancss.forum.repository
 
+import dev.alancss.forum.factory.CourseTestFactory
+import dev.alancss.forum.factory.TopicTestFactory
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import kotlin.test.assertEquals
 
 @DataJpaTest
 @Testcontainers
@@ -19,13 +25,21 @@ class TopicRepositoryTest {
     @Autowired
     private lateinit var topicRepository: TopicRepository
 
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
     companion object {
+        private const val POSTGRES_IMAGE = "postgres:latest"
+        private const val DB_NAME = "forum-testdb"
+        private const val DB_USERNAME = "username"
+        private const val DB_PASSWORD = "password"
+
         @JvmField
         @Container
-        val postgresqlContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
-            withDatabaseName("forum-testdb")
-            withUsername("username")
-            withPassword("password")
+        val postgresqlContainer = PostgreSQLContainer<Nothing>(POSTGRES_IMAGE).apply {
+            withDatabaseName(DB_NAME)
+            withUsername(DB_USERNAME)
+            withPassword(DB_PASSWORD)
         }
 
         @JvmStatic
@@ -37,8 +51,45 @@ class TopicRepositoryTest {
         }
     }
 
+    @BeforeEach
+    fun setUp() {
+        val course = courseRepository.save(CourseTestFactory.build(id = null))
+        val topic = TopicTestFactory.build(id = null, course = course)
+        topicRepository.save(topic)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        topicRepository.deleteAll()
+        courseRepository.deleteAll()
+    }
+
     @Test
     fun `should load context and repository`() {
         assertNotNull(topicRepository)
+    }
+
+    @Test
+    fun `should return topic by course name`() {
+        val result = topicRepository.findByCourseName(courseName = "Kotlin", pageable = PageRequest.of(0, 10))
+        val topic = result.content.first()
+
+        assertNotNull(result)
+        assertEquals(1, result.totalElements)
+
+        assertEquals("Kotlin", topic.course.name)
+        assertEquals("Error in Kotlin code", topic.title)
+        assertEquals("Var x is not defined in the current scope", topic.message)
+    }
+
+    @Test
+    fun `should generate report`() {
+        val report = topicRepository.report()
+
+        assertNotNull(report)
+
+        val dto = report.first()
+        assertEquals("Programming", dto.category)
+        assertEquals(1, dto.quantity)
     }
 }
